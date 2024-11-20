9c49c7ca283d308fd29de94fe0e4afb2b1f936fa
Deepa-2705 <deepagupta35795@gmail.com>
2024-11-21 02:14:47 +0530

    Project Setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHON_DOT_E='-e .'
+
+def get_requirements(file_paths:str)->List[str]:
+    requirements=[]
+    with open(file_paths)as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHON_DOT_E in requirements:
+            requirements.remove(HYPHON_DOT_E)
+
+        return requirements
+
+
+setup(
+    name='RealStateBusiness',
+    version='0.0.1',
+    author='Deepa Gupta',
+    email="deepagupta35795@gmail.com",
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
